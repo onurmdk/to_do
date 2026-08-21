5106f3054dc592afcbd233da12af207a00d028cc
Onur Madak <onurmdk@gmail.com>
2026-08-21 12:48:17 +0300

    Kullanici listeye eleman ekleyebiliyor.

--- script.js
+++ script.js
@@ -1,5 +1,45 @@
 //user butona basacak is ekle diye oradan prompt alacagiz sonra clickin icinde
 //tr olusturup gerekli infolari alip dolduracagiz ve status olacak
+function elemanEkle(to_do){
+    tablo.innerHTML = "";
+    const satir_h = document.createElement("tr");
+    tablo.appendChild(satir_h);
+    const h1 = document.createElement("th");
+    h1.textContent = "Title";
+    satir_h.appendChild(h1);
+    const h2 = document.createElement("th");
+    h2.textContent = "Description";
+    satir_h.appendChild(h2);
+    const h3 = document.createElement("th");
+    h3.textContent = "Critical Level";
+    satir_h.appendChild(h3);
+    const h4 = document.createElement("th");
+    h4.textContent = "Deadline Date";
+    satir_h.appendChild(h4);
+    const h5 = document.createElement("th");
+    h5.textContent = "Status";
+    satir_h.appendChild(h5);
+    to_do.forEach(madde => {
+        
+        const satir = document.createElement("tr");
+        tablo.appendChild(satir);
+        const s1 = document.createElement("td");
+        s1.textContent = madde.title;
+        satir.appendChild(s1);
+        const s2 = document.createElement("td");
+        s2.textContent = madde.description;
+        satir.appendChild(s2);
+        const s3 = document.createElement("td");
+        s3.textContent = madde.critical_lvl;
+        satir.appendChild(s3);
+        const s4 = document.createElement("td");
+        s4.textContent = madde.deadline_date;
+        satir.appendChild(s4);
+        const s5 = document.createElement("td");
+        s5.textContent = madde.status;
+        satir.appendChild(s5);
+    })
+}
 
 const to_do = [
     {
@@ -17,28 +57,27 @@ const to_do = [
         status:"Yapilacak"
     }
 ]
+elemanEkle(to_do);
 
-const tablo = document.querySelector("#tablo");
+const form = document.querySelector("#form");
+const f_title = document.querySelector("#f_title");
+const f_desc = document.querySelector("#f_desc");
+const f_crlvl = document.querySelector("#f_crlvl");
+const f_date = document.querySelector("#f_date");
+const f_status = document.querySelector("#f_status");
+const buton = document.querySelector("#buton");
+buton.addEventListener("click",function(e){
+    e.preventDefault();
+    const arr = {title: f_title.value,description: f_desc.value,critical_lvl: f_crlvl.value,deadline_date:f_date.value,status:f_status.value};
+    console.log(arr);
+    to_do.push(arr);
+    console.log(f_title.value);
+    console.log("tiklandi");
+    elemanEkle(to_do);
+});
 
-to_do.forEach(madde => {
-    const satir = document.createElement("tr");
-    tablo.appendChild(satir);
-    const s1 = document.createElement("td");
-    s1.textContent = madde.title;
-    satir.appendChild(s1);
-    const s2 = document.createElement("td");
-    s2.textContent = madde.description;
-    satir.appendChild(s2);
-    const s3 = document.createElement("td");
-    s3.textContent = madde.critical_lvl;
-    satir.appendChild(s3);
-    const s4 = document.createElement("td");
-    s4.textContent = madde.deadline_date;
-    satir.appendChild(s4);
-    const s5 = document.createElement("td");
-    s5.textContent = madde.status;
-    satir.appendChild(s5);
-})
+
+
 
 
 

fd76ef52e1c205a63b60aa816fd448e71552b2f0
Onur Madak <onurmdk@gmail.com>
2026-08-25 18:50:14 +0300

    Revize listesine gore guncellendi.

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ function elemanEkle(){
     yapilacaklar_icerik.innerHTML = "";
     devamEdiyor_icerik.innerHTML = "";
     tamamlananlar_icerik.innerHTML = "";
+    let todoCount = 0;
+    let inprogressCount = 0;
+    let completedCount = 0;
     to_do.forEach(madde => {
         const kart = document.createElement("div");
         kart.className = "kart";
@@ -9,6 +12,7 @@ function elemanEkle(){
         baslik.textContent = madde.title;
         kart.appendChild(baslik);
         const aciklama = document.createElement("p");
+        aciklama.className = "task_desc";
         aciklama.textContent = madde.description;
         kart.appendChild(aciklama);
         const seviye = document.createElement("p");
@@ -17,7 +21,9 @@ function elemanEkle(){
         const tarih = document.createElement("p");
         tarih.textContent = "Deadline Date: " + madde.deadline_date;
         kart.appendChild(tarih);
-
+        const divbuttons = document.createElement("div");
+        divbuttons.className = "div-buttons";
+        kart.appendChild(divbuttons);
         const editbuton = document.createElement("button");
         editbuton.textContent = "Edit";
         editbuton.className = "btn-edit";
@@ -31,32 +37,48 @@ function elemanEkle(){
             f_date.value = idli.deadline_date;
             f_status.value = idli.status;
         });
-        kart.appendChild(editbuton);
+        divbuttons.appendChild(editbuton);
         const silbuton = document.createElement("button");
         silbuton.textContent = "Delete";
         silbuton.className = "btn-delete";
         silbuton.addEventListener("click",function(e){
+            if(confirm("Are you sure you want to delete this task?") == false){
+                return;
+            }
             to_do = to_do.filter(liste => liste.id !== madde.id);
             kaydet();
             elemanEkle();
         });
-        kart.appendChild(silbuton);
+        divbuttons.appendChild(silbuton);
+        const addtaskbuton = document.createElement("button");
+        addtaskbuton.textContent = "Add New Task";
+        addtaskbuton.className = "btn-addtask";
+        addtaskbuton.addEventListener("click",function(e){
+
+        })
+        divbuttons.appendChild(addtaskbuton);
 
         let hedef;
         if(madde.status === "Yapilacak"){
             hedef = yapilacaklar_icerik;
+            todoCount++;
         } 
         else if(madde.status === "Devam Ediyor"){
             hedef = devamEdiyor_icerik;
+            inprogressCount++;
         } 
         else {
             hedef = tamamlananlar_icerik;
+            completedCount++;
         }
         if(madde.status === "Yapilacak") kart.classList.add("k-yapilacak");
         else if(madde.status === "Devam Ediyor") kart.classList.add("k-devam");
         else kart.classList.add("k-tamamlandi");
         hedef.appendChild(kart);
     });
+    k1.textContent = "To-Do (" + todoCount + ")";
+    k2.textContent = "In Progress (" + inprogressCount + ")";
+    k3.textContent = "Completed (" + completedCount + ")";
 }
 
 function kaydet(){
@@ -77,6 +99,9 @@ const liste = document.querySelector("#liste");
 const yapilacaklar_icerik = document.querySelector("#yapilacaklar_icerik");
 const devamEdiyor_icerik = document.querySelector("#devamEdiyor_icerik");
 const tamamlananlar_icerik = document.querySelector("#tamamlananlar_icerik");
+const k1 = document.querySelector("#k1");
+const k2 = document.querySelector("#k2");
+const k3 = document.querySelector("#k3");
 
 let to_do;
 let editId = null;
